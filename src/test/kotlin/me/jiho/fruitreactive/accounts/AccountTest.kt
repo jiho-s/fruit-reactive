501097365102584a755054ec468b704fc5478a0e
jiho-s <40516042+jiho-s@users.noreply.github.com>
2021-05-21 23:59:28 +0900

    test: Update AccountTest.kt

--- src/test/kotlin/me/jiho/fruitreactive/accounts/AccountTest.kt
+++ src/test/kotlin/me/jiho/fruitreactive/accounts/AccountTest.kt
@@ -2,11 +2,9 @@ package me.jiho.fruitreactive.accounts
 
 import me.jiho.fruitreactive.FruitReactiveApplication
 import me.jiho.fruitreactive.configurations.R2dbcConfiguration
+import org.junit.jupiter.api.*
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.BeforeAll
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.Assertions.assertNotEquals
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest
 import org.springframework.test.context.ContextConfiguration
@@ -14,14 +12,20 @@ import reactor.test.StepVerifier
 
 @DataR2dbcTest
 @ContextConfiguration(classes = [R2dbcConfiguration::class, FruitReactiveApplication::class])
+@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class AccountTest {
 
     @Autowired
     private lateinit var accountRepository: AccountRepository
 
+    private lateinit var account: Account
+
     @BeforeAll
     fun setUp() {
+        accountRepository.save(Account(email = "test@email.com", password = "pass")).subscribe{
+            account = it
+        }
     }
 
     @Test
@@ -37,5 +41,48 @@ internal class AccountTest {
             assertEquals(it.appleInBasket, 0)
             assertEquals(it.appleInTree, 0)
         }.verifyComplete()
+    }
+
+    @Test
+    @DisplayName("이메일로 조회 성공 테스트")
+    fun findByEmail_Success() {
+        val mono = accountRepository.findByEmail(account.email)
+
+        StepVerifier.create(mono).assertNext {
+            assertEquals(it.email, account.email)
+            assertEquals(it.password, account.password)
+            assertEquals(it.id, account.id)
+        }.verifyComplete()
+    }
+
+    @Test
+    @DisplayName("이메일로 조회 실패 테스트")
+    fun findByEmail_Fail() {
+        val mono = accountRepository.findByEmail("notvalid@email.com")
+
+        StepVerifier.create(mono).expectComplete().verify()
+    }
+
+    @Test
+    @DisplayName("id로 조회 성공 테스트")
+    fun findById_Success() {
+        val mono = accountRepository.findById(account.id!!)
+
+        StepVerifier.create(mono).assertNext {
+            assertEquals(it.id, account.id)
+            assertEquals(it.password, account.password)
+            assertEquals(it.email, account.email)
+        }.verifyComplete()
+    }
+
+    @Test
+    @DisplayName("account 변경 성공 테스트")
+    fun update_Success() {
+        accountRepository.save(Account(id = account.id, email = "after@email.com", password = "after"))
+            .`as`(StepVerifier::create).assertNext {
+                assertEquals(it.id, account.id)
+                assertNotEquals(it.email, account.email)
+                assertNotEquals(it.password, account.password)
+            }.verifyComplete()
     }
 }
