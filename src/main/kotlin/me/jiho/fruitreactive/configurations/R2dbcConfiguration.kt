cb47fe3054b4727a5cd5187b380dc906816c970a
jiho-s <40516042+jiho-s@users.noreply.github.com>
2021-05-21 19:50:32 +0900

    feat: Create R2dbcConfiguration.kt

---
+++ src/main/kotlin/me/jiho/fruitreactive/configurations/R2dbcConfiguration.kt
@@ -0,0 +1,23 @@
+package me.jiho.fruitreactive.configurations
+
+import io.r2dbc.spi.ConnectionFactory
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.io.ClassPathResource
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator
+
+@Configuration
+@EnableR2dbcRepositories
+class R2dbcConfiguration {
+
+    @Bean
+    fun initializer(connectionFactory: ConnectionFactory): ConnectionFactoryInitializer {
+        val initializer = ConnectionFactoryInitializer()
+        initializer.setConnectionFactory(connectionFactory)
+        initializer.setDatabasePopulator(ResourceDatabasePopulator(ClassPathResource("schema-h2.sql")))
+        return initializer
+    }
+
+}
