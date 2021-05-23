5414a1cd603517cb4015231cceed52067db83815
jiho-s <40516042+jiho-s@users.noreply.github.com>
2021-05-23 20:17:52 +0900

    feat: Create AccountService.kt

---
+++ src/main/kotlin/me/jiho/fruitreactive/accounts/AccountService.kt
@@ -0,0 +1,30 @@
+package me.jiho.fruitreactive.accounts
+
+import me.jiho.fruitreactive.errors.DuplicateException
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+
+@Service
+class AccountService(val accountRepository: AccountRepository) {
+
+    @Transactional(readOnly = true)
+    fun findById(id: Long): Mono<Account> = Mono.from(accountRepository.findById(id))
+
+    @Transactional(readOnly = true)
+    fun findByEmail(email: String): Mono<Account> = Mono.from(accountRepository.findByEmail(email))
+
+    @Transactional
+    fun create(email: String, password: String): Mono<Account> = validateEmail(email).handle { valid, sink ->
+        if (!valid) {
+            sink.error(DuplicateException(Account::class, email))
+        } else {
+            accountRepository.save(Account(email = email, password = password))
+        }
+    }
+
+    @Transactional(readOnly = true)
+    fun validateEmail(email: String) : Mono<Boolean> =
+        Mono.from(accountRepository.findByEmail(email)).hasElement().map { !it }
+}
