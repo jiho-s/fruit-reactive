122b3ea621fde9feb1de662c1b1f548ae83be260
jiho-s <40516042+jiho-s@users.noreply.github.com>
2021-05-24 13:43:14 +0900

    feat: Create ApiResult.kt

---
+++ src/main/kotlin/me/jiho/fruitreactive/commons/ApiResult.kt
@@ -0,0 +1,14 @@
+package me.jiho.fruitreactive.commons
+
+class ApiResult<T: Any> private constructor(val success: Boolean, val response: T? = null, val error: ApiError? = null) {
+    companion object {
+        @JvmStatic
+        fun<T: Any> success(response: T): ApiResult<T> = ApiResult(success = true, response = response)
+    }
+
+    override fun toString(): String {
+        return "ApiResult(success=$success, response=$response, error=$error)"
+    }
+
+
+}
