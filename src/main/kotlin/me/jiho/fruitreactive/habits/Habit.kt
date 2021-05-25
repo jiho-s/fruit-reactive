8306860161c23488c0f2bdd94736841332ff0a80
jiho-s <40516042+jiho-s@users.noreply.github.com>
2021-05-25 16:14:46 +0900

    feat: Update Habit
    description can null

--- src/main/kotlin/me/jiho/fruitreactive/habits/Habit.kt
+++ src/main/kotlin/me/jiho/fruitreactive/habits/Habit.kt
@@ -2,7 +2,7 @@ package me.jiho.fruitreactive.habits
 
 import org.springframework.data.annotation.Id
 
-data class Habit(@Id val id: Long? = null, val name: String, val description: String, val accountId: Long?) {
+data class Habit(@Id val id: Long? = null, val name: String, val description: String?, val accountId: Long?) {
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
