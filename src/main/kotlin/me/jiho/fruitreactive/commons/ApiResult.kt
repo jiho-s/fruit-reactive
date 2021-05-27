90cbe8e015409d6f8e9f75124c620f41fa2960ed
jiho-s <40516042+jiho-s@users.noreply.github.com>
2021-05-27 14:07:35 +0900

    feat: Update ApiResult.kt
    success method T can null

--- src/main/kotlin/me/jiho/fruitreactive/commons/ApiResult.kt
+++ src/main/kotlin/me/jiho/fruitreactive/commons/ApiResult.kt
@@ -3,7 +3,7 @@ package me.jiho.fruitreactive.commons
 class ApiResult<T: Any> private constructor(val success: Boolean, val response: T? = null, val error: ApiError? = null) {
     companion object {
         @JvmStatic
-        fun<T: Any> success(response: T): ApiResult<T> = ApiResult(success = true, response = response)
+        fun<T: Any> success(response: T?): ApiResult<T> = ApiResult(success = true, response = response)
     }
 
     override fun toString(): String {
